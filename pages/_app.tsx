b6d94f5016012fd00c4ed1249be5629590333312
Timetravelapes <102059377+Timetravelapes@users.noreply.github.com>
2022-12-28 21:09:21 +0100

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import Head from "next/head";
+import ThirdwebGuideFooter from "../components/GitHubLink";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Avalanche;
@@ -8,7 +10,20 @@ const activeChainId = ChainId.Avalanche;
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head>
+        <title>Timetravelapes Minting Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="NFT FARM dApp TIMETRAVELAPES"
+        />
+        <meta
+          name="keywords"
+          content="NFT, defi, mint, crypto, AVAX "
+        />
+      </Head>
       <Component {...pageProps} />
+      <ThirdwebGuideFooter />
     </ThirdwebProvider>
   );
 }
